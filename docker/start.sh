319d9f5dad42652e6711e704880b7a6ee9889ad3
Vinicius Zynich <viniciuscardosozynich@gmail.com>
2026-06-14 21:43:54 -0300

    config prod

---
+++ docker/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+php artisan storage:link --quiet 2>/dev/null || true
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan migrate --force
+
+php-fpm -D
+exec nginx -g "daemon off;"
